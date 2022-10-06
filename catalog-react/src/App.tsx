450767576c751843a3bbf0e6c54e89b4b91b50e7
kluivert.queiroz <kluivert.queiroz@hotmail.com>
2022-10-05 21:55:41 -0300

    change background color

--- catalog-react/src/App.tsx
+++ catalog-react/src/App.tsx
@@ -5,12 +5,14 @@ import { CartContext } from "./contexts/CartContext";
 import { CREATE_EMPTY_CART } from "./graphql/carts";
 import Catalogue from "./pages/Catalogue";
 import { Cart } from "./types/cart";
+import './index.css'
 function App() {
   const [createCart, { data: emptyCartData }] = useMutation<{
     createCart: Cart;
   }>(CREATE_EMPTY_CART);
   useEffect(() => {
     createCart();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   if (!emptyCartData?.createCart) return <div>Loading</div>;
   return (
